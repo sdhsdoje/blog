91e961663622361b38c28378094fc75fc2ac6678
sdhsdoje <30852155+sdhsdoje@users.noreply.github.com>
2017-08-16 17:01:30 +0900

    13
    13

--- bindex.php
+++ bindex.php
@@ -3,7 +3,68 @@ session_start();
 $url=$_SERVER['PHP_SELF'];
 $surl=explode('/',$url);
 $id=$surl[2];
-?>
+
+if($_SESSION['name']==$id){
+    ?>
+    <!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $id."님의 블로그"?></title>
+    <link rel="stylesheet" href="../css/cssb.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+</head>
+<body>
+    <div id="wrap">
+        <header>
+             
+        </header>
+        <div id="content">
+            
+        </div>
+        <div id="content2">
+            <div id="blog_name">
+                <?php echo $id ?>
+            </div>
+        </div>
+        <div id="content3">     
+            <section>
+               <?php
+        $con=mysqli_connect("localhost","root","kjh","project");
+        $query="select * from board where name='$id'";
+        $info=mysqli_query($con,$query);
+        while($re=mysqli_fetch_array($info)){
+                ?>
+                <div id="st">
+                    <a href='<?php echo $re['idx'] ?>'>
+                        <h1><?php echo $re['title'] ?></h1>
+                        <h5><?php echo $re['content'] ?></h5>
+                    </a>
+                </div>
+                <?php
+        }
+    ?>
+            </section>
+            <article>
+                <a href="../write.html" id="button">글쓰기</a>
+                <div id="list">
+                    <strong>최근 글</strong><br>
+                    
+                </div>
+            </article>
+        </div>
+        <footer>
+           <br><br>
+            center this is KJH!
+        </footer>
+    </div>
+</body>
+</html>
+    <?php
+}
+else
+{
+    ?>
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -24,12 +85,6 @@ $id=$surl[2];
             <div id="blog_name">
                 <?php echo $id ?>
             </div>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-            </ul>
         </div>
         <div id="content3">     
             <section>
@@ -66,3 +121,6 @@ $id=$surl[2];
     </div>
 </body>
 </html>
+<?php
+}
+?>
--- write.php
+++ write.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 session_start();
 if(!isset($_SESSION['id'])){
     ?>
@@ -9,13 +11,19 @@ location.href="index.php"
 <?php
     exit();
 }
-$title=mysql_real_escape_string($_POST['title']);
-$content=mysql_real_escape_string($_POST['content']);
 $name=$_SESSION['name'];
 $hash_id=hash('sha512',$_SESSION['id']);
+$title=$_POST['title'];
+$content=$_POST['content'];
 $con=mysqli_connect("localhost","root","kjh","project") or die ("fail to connect");
 $query="insert into board (title,content,name,id,time,idx) values('$title','$content','$name','$hash_id','','')";
-mysqli_query($con,$query);
+mysqli_query($con,$query) or die("fail query");
+echo "
+<script>
+alert('글이 성공적으로 저장되었습니다.');
+location.href='$name/index.php';
+</script>
+"
 ?>
 <script>
 alert("글이 성공적으로 저장되었습니다.");
